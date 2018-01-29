ed5d61aee43afb662b4d3244f8e57b33016455b3
Mariusz <mariusz.blaszczak@gmail.com>
2018-01-29 14:13:19 +0100

    test 3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,16 +1,20 @@
 class Bottles
   def verse(verse)
-    case verse
-      when 99
-        <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-        VERSE
-      when 89
-        <<-VERSE
-89 bottles of beer on the wall, 89 bottles of beer.
-Take one down and pass it around, 88 bottles of beer on the wall.
-        VERSE
+    generate_verse_for(verse)
+  end
+
+  def generate_verse_for(number_of_bottles)
+    decremented_number_of_bottles = number_of_bottles - 1
+    if decremented_number_of_bottles > 1
+      <<-VERSE
+#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
+Take one down and pass it around, #{decremented_number_of_bottles} bottles of beer on the wall.
+      VERSE
+    else
+      <<-VERSE
+#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
+Take one down and pass it around, #{decremented_number_of_bottles} bottle of beer on the wall.
+      VERSE
     end
   end
 end
