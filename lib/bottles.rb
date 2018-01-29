a320b658bfcdda1a3a4ca14bec88d72fe793e727
Mariusz <mariusz.blaszczak@gmail.com>
2018-01-29 14:05:20 +0100

    test 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
-  def verse(arg)
-    <<-VERSE
+  def verse(verse)
+    case verse
+      when 99
+        <<-VERSE
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
-    VERSE
+        VERSE
+      when 89
+        <<-VERSE
+89 bottles of beer on the wall, 89 bottles of beer.
+Take one down and pass it around, 88 bottles of beer on the wall.
+        VERSE
+    end
   end
 end
