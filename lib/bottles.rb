78e39b105daa3aa618a23ccc24c5c6297b92afae
Mariusz <mariusz.blaszczak@gmail.com>
2018-01-29 14:26:11 +0100

    test 4

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,16 +5,23 @@ class Bottles
 
   def generate_verse_for(number_of_bottles)
     decremented_number_of_bottles = number_of_bottles - 1
-    if decremented_number_of_bottles > 1
-      <<-VERSE
-#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-Take one down and pass it around, #{decremented_number_of_bottles} bottles of beer on the wall.
-      VERSE
-    else
-      <<-VERSE
-#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-Take one down and pass it around, #{decremented_number_of_bottles} bottle of beer on the wall.
-      VERSE
+
+    number_of_bottles_string = number_to_bottles(number_of_bottles)
+    decremented_number_of_bottles_string = number_to_bottles(decremented_number_of_bottles)
+    take_one_down_part = (number_of_bottles == 1) ? "Take it down" : "Take one down"
+    <<-VERSE
+#{number_of_bottles_string} of beer on the wall, #{number_of_bottles_string} of beer.
+#{take_one_down_part} and pass it around, #{decremented_number_of_bottles_string} of beer on the wall.
+    VERSE
+  end
+
+  def number_to_bottles(number)
+    if number > 1
+      "#{number} bottles"
+    elsif number == 1
+      "#{number} bottle"
+    elsif number == 0
+      "no more bottles"
     end
   end
 end
