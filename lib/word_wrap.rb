9d6771bc52473b48113e90386c36cb2f6282ca76
Mariusz <mariusz.blaszczak@gmail.com>
2018-07-03 07:37:01 +0200

    add word wrap simple steps. step 6

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -1,15 +1,14 @@
 class WordWrap
   def self.wrap(text, number)
-    if number < text.length
-      if text[number] == ' '
-        return text.slice(0, number) + "\n" + text.slice(number + 1, 100000)
-      elsif text[0..number].include?(" ")
-        new_number = text.index(" ")
-        return text.slice(0, new_number) + "\n" + wrap(text.slice(new_number+1, 100000), number)
-      end
+    return text if number >= text.length
 
-      return text.slice(0, number) + "\n" + wrap(text.slice(number, 100000), number)
+    if text[number] == ' '
+      return text.slice(0, number) + "\n" + text.slice(number + 1, 100000)
+    elsif text[0..number].include?(" ")
+      new_number = text.index(" ")
+      return text.slice(0, new_number) + "\n" + wrap(text.slice(new_number + 1, 100000), number)
     end
-    text
+
+    text.slice(0, number) + "\n" + wrap(text.slice(number, 100000), number)
   end
 end
