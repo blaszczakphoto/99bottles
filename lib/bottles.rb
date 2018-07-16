a3b4f50fb3819a14b3bdbdd78cd55563e0d857c9
Mariusz <mariusz.blaszczak@gmail.com>
2018-07-16 08:12:02 +0200

    split to separate verses, as it is in the examples

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,14 +2,20 @@ class Bottles
   def verse(number)
     case number
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down, pass it around, 1 bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down, pass it around, " +
+      "1 bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down, pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down, pass it around, " +
+      "#{number - 1} bottles of beer on the wall.\n"
     end
   end
 end
