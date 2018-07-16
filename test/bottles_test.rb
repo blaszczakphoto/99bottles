9dc8505205dac9a0c5ec840dfe8e7f5a3d2baa57
Mariusz <mariusz.blaszczak@gmail.com>
2018-07-16 09:09:34 +0200

    finish verses method implementation

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -17,7 +17,7 @@ class BottlesTest < Minitest::Test
 
   def test_verse_2
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down, pass it around, 1 bottle of beer on the wall.\n"
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
   end
 
@@ -33,5 +33,36 @@ class BottlesTest < Minitest::Test
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(0)
+  end
+
+  def test_a_couple_verses
+    expected = "99 bottles of beer on the wall, " +
+      "99 bottles of beer.\n" +
+      "Take one down, pass it around, " +
+      "98 bottles of beer on the wall.\n" +
+      "\n" +
+      "98 bottles of beer on the wall, " +
+      "98 bottles of beer.\n" +
+      "Take one down, pass it around, " +
+      "97 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verses(99,98)
+  end
+
+  def test_a_few_verses
+    expected = "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n" +
+      "\n" +
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n" +
+      "\n" +
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verses(2, 0)
   end
 end
