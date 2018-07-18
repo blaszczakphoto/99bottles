e8b0d4b17117057b8b1dfab64cc28f4e9e342d1f
Mariusz <mariusz.blaszczak@gmail.com>
2018-07-18 11:12:38 +0200

    extract class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -17,6 +17,30 @@ class Bottles
   end
 
   def container(number)
+    BottleNumber.new(number).container
+  end
+
+  def quantity(number)
+    BottleNumber.new(number).quantity
+  end
+
+  def action(number = 0)
+    BottleNumber.new(number).action
+  end
+
+  def successor(number)
+    BottleNumber.new(number).successor
+  end
+end
+
+
+class BottleNumber
+  attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def container
     if number == 1
       'bottle'
     else
@@ -24,7 +48,7 @@ class Bottles
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       'it'
     else
@@ -32,7 +56,7 @@ class Bottles
     end
   end
 
-  def quantity(number)
+  def quantity
     if number == 0
       'no more'
     else
@@ -40,15 +64,15 @@ class Bottles
     end
   end
 
-  def action(number = 0)
+  def action
     if number == 0
       'Go to the store and buy some more, '
     else
-      "Take #{pronoun(number)} down and pass it around, "
+      "Take #{pronoun} down and pass it around, "
     end
   end
 
-  def successor(number)
+  def successor
     if number == 0
       99
     else
